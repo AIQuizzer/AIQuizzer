4a982319e55a6331535cf01a5709e3eabf1ac6b3
Willaiem <vvwill4u@gmail.com>
2023-09-14 18:00:09 +0200

    Feat: add find lobby component and user icon placeholder

--- src/pages/FindLobby.tsx
+++ src/pages/FindLobby.tsx
@@ -1,112 +1,94 @@
+import { useMutation, useQuery } from "convex/react"
+import { api } from "../../convex/_generated/api"
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
 import { Button } from "../ui/button"
 
-const lobbies = [
-	{
-		id: "1",
-		name: "Lobby 1",
-		players: [
-			{
-				id: "1",
-				name: "Player 1",
-				img: "https://picsum.photos/200",
-			},
-			{
-				id: "2",
-				name: "Player 2",
-				img: "https://picsum.photos/200",
-			},
-			{
-				id: "3",
-				name: "Player 1",
-				img: "https://picsum.photos/200/",
-			},
-			{
-				id: "4",
-				name: "Player 2",
-				img: "https://picsum.photos/200",
-			},
-			{
-				id: "5",
-				name: "Player 1",
-				img: "https://picsum.photos/200/",
-			},
-		],
-		joinedPlayers: 3,
-		maxPlayers: 5,
-	},
-	{
-		id: "2",
-		name: "Lobby 1",
-		players: [
-			{
-				id: "1",
-				name: "Player 1",
-				img: "https://picsum.photos/200",
-			},
-			{
-				id: "2",
-				name: "Player 2",
-				img: "https://picsum.photos/200",
-			},
-			{
-				id: "3",
-				name: "Player 1",
-				img: "https://picsum.photos/200/",
-			},
-			{
-				id: "4",
-				name: "Player 2",
-				img: "https://picsum.photos/200",
-			},
-			{
-				id: "5",
-				name: "Player 1",
-				img: "https://picsum.photos/200/",
-			},
-		],
-		joinedPlayers: 3,
-		maxPlayers: 5,
-	},
-]
+import { useAuth0 } from "@auth0/auth0-react"
+import { useNavigate } from "react-router-dom"
+import userPlaceholder from "../assets/userplaceholder.png"
+import { LoadingSpinner } from "../components/LoadingSpinner"
+
+const Lobbies = () => {
+	const { user } = useAuth0()
+	const joinLobby = useMutation(api.mutations.joinLobby)
+	const lobbies = useQuery(api.queries.getLobbies)
+	const navigate = useNavigate()
+
+	if (!lobbies) {
+		return <LoadingSpinner />
+	}
+
+	if (lobbies.length === 0) {
+		return (
+			<div className="flex flex-col items-center justify-center gap-8">
+				<h2 className="text-3xl font-semibold">No categories found</h2>
+				<Button onClick={() => navigate("/categories")}>
+					Create a new one!
+				</Button>
+			</div>
+		)
+	}
+
+	return (
+		<ul className="flex flex-col items-center justify-center gap-4">
+			{lobbies.map((lobby) => (
+				<li
+					key={lobby._id}
+					className="border-20 flex items-center justify-center gap-14 rounded-lg bg-neutral-200 p-4"
+				>
+					<h2>{lobby.name}</h2>
+					<div className="flex gap-2">
+						{Array.from({ length: lobby.maxPlayers }, (_, index) => {
+							const player = lobby.players[index] ?? {
+								id: index,
+								img: userPlaceholder,
+								name: `Player ${index + 1}`,
+							}
+
+							return (
+								<Avatar key={player.id}>
+									<AvatarImage src={player.img} />
+									<AvatarFallback>{player.name}</AvatarFallback>
+								</Avatar>
+							)
+						})}
+					</div>
+					<p
+						aria-label={`Joined ${lobby.joinedPlayers} players out of ${lobby.maxPlayers}`}
+					>
+						{lobby.joinedPlayers} / {lobby.maxPlayers}
+					</p>
+
+					<Button
+						disabled={lobby.joinedPlayers === lobby.maxPlayers}
+						onClick={async () => {
+							if (!user?.sub || !user?.name || !user?.picture) return
+
+							await joinLobby({
+								lobbyId: lobby._id,
+								player: {
+									id: user.sub,
+									name: user.name,
+									img: user.picture,
+								},
+							})
+
+							navigate(`/lobby/${lobby._id}`)
+						}}
+					>
+						Join
+					</Button>
+				</li>
+			))}
+		</ul>
+	)
+}
 
 export const FindLobby = () => {
-	const filteredLobbies = lobbies.filter(
-		(lobby) => lobby.joinedPlayers < lobby.maxPlayers,
-	)
-
 	return (
 		<div className="flex flex-col items-center justify-center gap-8">
 			<h1>Find Lobby</h1>
-			<ul className="flex flex-col items-center justify-center gap-4">
-				{filteredLobbies.map((lobby) => {
-					const isFull = lobby.joinedPlayers === lobby.maxPlayers
-
-					return (
-						<li
-							key={lobby.id}
-							className="border-20 flex items-center justify-center gap-14 rounded-lg bg-neutral-200 p-4"
-						>
-							<h2>{lobby.name}</h2>
-							<div className="flex gap-2">
-								{lobby.players.map((player) => (
-									<Avatar key={player.id}>
-										<AvatarImage src={player.img} />
-										<AvatarFallback>{player.name}</AvatarFallback>
-									</Avatar>
-								))}
-							</div>
-							<p
-								aria-label={`Joined ${lobby.joinedPlayers} players out of ${lobby.maxPlayers}`}
-							>
-								{lobby.joinedPlayers} / {lobby.maxPlayers}
-							</p>
-
-							<Button disabled={isFull}>Join</Button>
-						</li>
-					)
-				})}
-			</ul>
+			<Lobbies />
 		</div>
 	)
 }
