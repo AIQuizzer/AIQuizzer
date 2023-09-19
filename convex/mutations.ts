4250018ad34171a52ef1d721746a4b3fc7412e5c
Michał Kaniowski <kaniowskimichal2@gmail.com>
2023-09-19 11:26:02 +0200

    feat: add leave lobby functionality

--- convex/mutations.ts
+++ convex/mutations.ts
@@ -47,3 +47,38 @@ export const joinLobby = mutation({
 		})
 	},
 })
+
+export const leaveLobby = mutation({
+	args: {
+		lobbyId: v.id("lobbies"),
+		playerId: v.string(),
+	},
+	handler: async (ctx, args) => {
+		const lobby = await ctx.db.get(args.lobbyId)
+
+		if (!lobby) {
+			throw new Error("Lobby not found")
+		}
+
+		const playerInLobby = lobby.players.find(
+			(player) => player.id === args.playerId,
+		)
+
+		if (!playerInLobby) {
+			throw new Error("Player not in the lobby")
+		}
+
+		if (lobby.players.length === 1) {
+			await ctx.db.delete(args.lobbyId)
+			return
+		}
+
+		const filteredPlayers = lobby.players.filter(
+			(player) => player.id !== args.playerId,
+		)
+
+		await ctx.db.patch(args.lobbyId, {
+			players: filteredPlayers,
+		})
+	},
+})
