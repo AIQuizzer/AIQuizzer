459dda09f6ce6ee16c3dd4a0ff64b8b38a4a2090
Michał Kaniowski <kaniowskimichal2@gmail.com>
2023-09-19 20:09:47 +0200

    feat: add joining to lobby by link

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -1,4 +1,4 @@
-import { withAuthenticationRequired } from "@auth0/auth0-react"
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react"
 import { useAction, useQuery, useMutation } from "convex/react"
 import { useEffect, useState } from "react"
 import { api } from "../../convex/_generated/api"
@@ -6,7 +6,7 @@ import { Question } from "../../convex/quiz"
 import { Lobby } from "../components/Lobby"
 import { Quiz } from "../components/Quiz"
 import { Redirect } from "../components/Redirect"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { Id } from "../../convex/_generated/dataModel"
 
 const DUMMY_ANSWERS = [
@@ -44,15 +44,44 @@ const DUMMY_QUESTIONS = [
 export function _Lobby() {
 	const [questions, setQuestions] = useState<Question[]>(DUMMY_QUESTIONS)
 	const [hasStarted, setHasStarted] = useState(false)
-	const getQuestions = useAction(api.quiz.getQuestions)
+
+	const navigate = useNavigate()
 	const params = useParams()
 	const lobbyId = params.lobbyId as Id<"lobbies">
+
+	const getQuestions = useAction(api.quiz.getQuestions)
 	const createGame = useMutation(api.mutations.createGame)
 	const lobby = useQuery(api.queries.getLobby, { lobbyId: lobbyId! })
+	const joinLobby = useMutation(api.mutations.joinLobby)
+
+	const { user } = useAuth0()
 
 	useEffect(() => {
+		// start game for all users in that lobby
 		if (lobby?.gameId) {
 			setHasStarted(true)
+		}
+
+		// user can join by pasting link
+		if (lobby) {
+			const playerInLobby = lobby.players.find(
+				(player) => player.id === user?.sub,
+			)
+
+			if (!playerInLobby) {
+				if (!user) {
+					navigate("/")
+				}
+
+				joinLobby({
+					lobbyId,
+					player: {
+						id: user?.sub || "",
+						img: user?.picture || "",
+						name: user?.name || "",
+					},
+				})
+			}
 		}
 	}, [lobby])
 
